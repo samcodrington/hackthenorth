0ec13a15978b57ddd7b66b900eb89a3ce1f8168b
Sam Codrington <sam.codrington@queensu.ca>
2017-09-16 08:29:43 -0400

    merge

--- app/assets/scripts/modules/DropZone.js
+++ app/assets/scripts/modules/DropZone.js
@@ -1,14 +1,94 @@
 import $ from 'jquery';
 
+
+// Drag and drop code primarily from:
+// https://css-tricks.com/drag-and-drop-file-uploading/
+var fD;
 class DropZone {
 
     constructor(featureDetector) {
 
+        fD = featureDetector;
         this.$form = $('.drop-zone');
+        this.$input = $('#file');
 
-        if (featureDetector.advancedUpload) {
+        this.enableInputAutoSubmit();
+
+        if (fD.advancedUpload) {
             this.$form.addClass('has-advanced-upload');
+
+            this.enableDragAndDrop();
         }
+
+        this.enableSubmitEvent();
+    }
+
+    enableInputAutoSubmit() {
+        this.$input.on('change', (event) => {
+            this.$form.trigger('submit');
+        });
+    }
+
+    enableDragAndDrop() {
+        var droppedFiles = false;
+        this.$form.on('drag dragstart dragend dragover dragenter dragleave drop', (event) => {
+            event.preventDefault();
+            event.stopPropagation();
+        })
+        .on('dragover dragenter', () => {
+            this.$form.addClass('is-dragover');
+        })
+        .on('dragleave dragend drop', () => {
+            this.$form.removeClass('is-dragover');
+        })
+        .on('drop', (event) => {
+            this.droppedFiles = event.originalEvent.dataTransfer.files;
+            this.$form.trigger('submit');
+        });
+    }
+
+    enableSubmitEvent() {
+        this.$form.on('submit', (event) => {
+            if (this.$form.hasClass('is-uploading')) return false;
+
+            this.$form.addClass('is-uploading').removeClass('is-error');
+
+            if (fD.advancedUpload) {
+                // Ajax for modern browsers
+                event.preventDefault();
+
+                var ajaxData = new FormData(this.$form.get(0));
+
+                if (this.droppedFiles) {
+                    $.each(this.droppedFiles, function(i, file) {
+                        ajaxData.append(this.$input.attr('name'), file);
+                    }.bind(this));
+                }
+
+                $.ajax({
+                    url: this.$form.attr('action'),
+                    type: this.$form.attr('method'),
+                    data: ajaxData,
+                    dataType: 'json',
+                    cache: false,
+                    contentType: false,
+                    processData: false,
+                    complete: () => {
+                        this.$form.removeClass('is-uploading');
+                    },
+                    success: (data) => {
+                        this.$form.addClass(data.success == true ? 'is-success' : 'is-error');
+                        if (!data.success) console.error(data.error); //TODO: replace with a visible span/div
+                    },
+                    error: () => {
+                        //TODO: log, alert, etc.
+                    }
+                });
+            } else {
+                // TODO: ajax for legacy browsers
+            }
+
+        });
     }
 
 }
