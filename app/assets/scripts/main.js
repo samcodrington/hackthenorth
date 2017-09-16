618bcb61972869a36295c02d24d4fbbe715e62a5
Sam Codrington <sam.codrington@queensu.ca>
2017-09-16 02:56:15 -0400

    fix url button, add beginning of face

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,3 +1,7 @@
 import imageProcessor from './modules/ImageProcessor';
+import $ from 'jquery';
 
 var iP = new imageProcessor();
+$('#urlbutton').on('click', function(){
+    iP.processImage();
+});
