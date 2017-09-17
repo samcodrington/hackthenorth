0b9bc82cbcd1ecadbf502884ce482caa24ec60cb
Ben <benvenda@gmail.com>
2017-09-17 00:03:09 -0400

    Remove old drop zone code.

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,6 +1,5 @@
 import ImageProcessor from './modules/ImageProcessor';
 import FeatureDetector from './modules/FeatureDetector';
-// import DropZone from './modules/DropZone';
 import DatabaseManager from './modules/DatabaseManager';
 import $ from 'jquery';
 
@@ -9,5 +8,4 @@ $('#urlbutton').on('click', function(){
     iP.processImage();
 });
 var fD = new FeatureDetector();
-// var dZ = new DropZone(fD, iP);
 var dM = new DatabaseManager();
