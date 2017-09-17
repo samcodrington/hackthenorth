ea8374a3909c82c1d2c7852d1cb8f71d1dd62a15
Ben <benvenda@gmail.com>
2017-09-17 00:01:38 -0400

    Implement unique actor id function.

--- app/assets/scripts/modules/movieMatch.js
+++ app/assets/scripts/modules/movieMatch.js
@@ -1,9 +1,9 @@
-
 let api_key = '190078ca8ad2919e5e468521e5d5114a';
 let uri_root = 'https://api.themoviedb.org/3/';
 
 class movieMatch{
     constructor(actorTMDBids){
+        actorTMDBids = this.removeDoubles(actorTMDBids);
         this.findMovies(actorTMDBids);
     }
     findMovies(actorTMDBids){
@@ -27,5 +27,11 @@ class movieMatch{
         query.api_key = api_key;
         var url = uri_root + "/person/" + actorId + "/movie_credits"
 
+    }
+    removeDoubles(tmdbIds) {
+        var seen = {};
+        return tmdbIds.filter(function(item) {
+            return seen.hasOwnProperty(item) ? false : seen([item] = true);
+        });
     }
 }
